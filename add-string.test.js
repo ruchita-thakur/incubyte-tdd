5b58f0e38ffad35a77ef33684e93dd1dbba420a8
rohit-thakur12 <thakurruchita24@gmail.com>
2024-09-27 19:47:55 +0530

    added test cases to add multiple numbers

--- add-string.test.js
+++ add-string.test.js
@@ -7,4 +7,10 @@ describe("add", () => {
   it("should return the number itself for a single number string", () => {
     expect(add("1")).toBe(1);
   });
+  it("should return the sum of multiple numbers", () => {
+    expect(add("1,2,3,4")).toBe(10);
+  });
+  it("should return a number", () => {
+    expect(add("1,2,3")).toEqual(expect.any(Number));
+  });
 });
